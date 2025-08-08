c0f7683ebf99fdc4bf766a5c28c9b89adbf7d0c5
Leonardo Amaral <leonardo_amaral98@hotmail.com>
2025-08-08 17:21:28 -0300

    Adicionado aviso de licença ausente no arquivo core.sh

--- usr/lib/debian-nvidia-installer/tui/core.sh
+++ usr/lib/debian-nvidia-installer/tui/core.sh
@@ -1,5 +1,23 @@
 #!/usr/bin/env bash
 
+# debian-nvidia-installer - NVIDIA Driver Installer for Debian (TUI)
+# Copyright (C) 2025 Leonardo Amaral
+#
+# This file is part of debian-nvidia-installer.
+#
+# debian-nvidia-installer is free software: you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# debian-nvidia-installer is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with debian-nvidia-installer. If not, see <https://www.gnu.org/licenses/gpl-3.0.html>.
+
 # NAVIGATION_STATUS 0 : Navegação encerrada
 # NAVIGATION_STATUS 1 : Navegação em andamento
 declare -ig NAVIGATION_STATUS=1
