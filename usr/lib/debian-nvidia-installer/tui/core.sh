9e7f0b4be30588c09244134f486be6b0912da247
Leonardo Amaral <leonardo_amaral98@hotmail.com>
2025-08-08 17:58:50 -0300

    Melhoria na forma que a TUI é exibida e em como o valor de retorno é capturado

--- usr/lib/debian-nvidia-installer/tui/core.sh
+++ usr/lib/debian-nvidia-installer/tui/core.sh
@@ -24,58 +24,59 @@ declare -ig NAVIGATION_STATUS=1
 
 # Configurações globais do Dialog
 DIALOG_OPTS=(
-    --clear
     --keep-tite
 )
 
 # Menu com multiplas escolhas sem botão de cancelamento
 tui::show_menu() {
+    local choice
+
     local title="$1"
     local prompt="$2"
     shift 2
     local menu_items=("$@")
     
-    dialog "${DIALOG_OPTS[@]}" \
+    choice="$(dialog "${DIALOG_OPTS[@]}" \
            --no-cancel \
-           --stdout \
            --title "$title" \
+           --ok-label "$(tr::t "tui.confirm")" \
            --menu "$prompt" \
            15 50 10 \
-           "${menu_items[@]}"
+           "${menu_items[@]}" 2>&1 1>/dev/tty)"
 
-    return $?
+    local ret="$?"
+    echo "$choice"
+    return "$ret"
 }
 
 # Caixa de dialogo sem botão de cancelamento
 tui::show_msgbox() {
     local title="$1"
     local message="$2"
-    local ok_label="${3:-OK}"
+    local ok_label="${3:-"$(tr::t "tui.ok")"}"
 
     dialog "${DIALOG_OPTS[@]}" \
            --no-cancel \
-           --stdout \
            --title "$title" \
            --ok-label "$ok_label" \
            --msgbox "$message" \
-           15 60
+           15 60 1>/dev/tty
 
-    return $?
+    return "$?"
 }
 
 # Caixa de dialogo de dupla escolha
 tui::show_yesno() {
     local title="$1"
     local message="$2"
-    local yes_label="${3:-Sim}"
-    local no_label="${4:-Não}"
+    local yes_label="${3:-"$(tr::t "tui.confirm")"}"
+    local no_label="${4:-"$(tr::t "tui.cancel")"}"
 
     dialog "${DIALOG_OPTS[@]}" \
-           --stdout \
            --title "$title" \
            --yes-label "$yes_label" \
            --no-label "$no_label" \
-           --yesno "$message" 15 50
-
-    return $?
+           --yesno "$message" 15 50 1>/dev/tty
+    
+    return "$?"
 }
