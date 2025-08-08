b8368cb65622cb5e607d59198c58f6ad32835f77
Leonardo Amaral <leonardo_amaral98@hotmail.com>
2025-08-08 17:17:21 -0300

    Adiciona um pipe (&3) dedicado para logging, permitindo gravar logs sem formtação com caracteres de escape ANSI.

--- usr/lib/debian-nvidia-installer/logging.sh
+++ usr/lib/debian-nvidia-installer/logging.sh
@@ -49,25 +49,25 @@ log::setup_logger() {
 # Log info (saída padrão)
 log::info() {
     echo -e "[$(date "+%Y-%m-%d %H:%M:%S")] Info: $1" >&3
-    echo -e "${LOG_ESC_BOLD_BLUE}Info:${LOG_ESC_RESET} $1" >&2
+    echo -e "${LOG_ESC_BOLD_BLUE}Info:${LOG_ESC_RESET} $1"
 }
 
 # Log warning (saída de erro)
 log::warn() {
     echo -e "[$(date "+%Y-%m-%d %H:%M:%S")] Warn: $1" >&3
-    echo -e "${LOG_ESC_BOLD_YELLOW}Warn:${LOG_ESC_RESET}${LOG_ESC_BOLD} $1 ${LOG_ESC_RESET}" >&2
+    echo -e "${LOG_ESC_BOLD_YELLOW}Warn:${LOG_ESC_RESET}${LOG_ESC_BOLD} $1 ${LOG_ESC_RESET}"
 }
 
 # Log error (saída de erro)
 log::error() {
     echo -e "[$(date "+%Y-%m-%d %H:%M:%S")] Error: $1" >&3
-    echo -e "${LOG_ESC_BOLD_RED}Error:${LOG_ESC_RESET}${LOG_ESC_BOLD} $1 ${LOG_ESC_RESET}" >&2
+    echo -e "${LOG_ESC_BOLD_RED}Error:${LOG_ESC_RESET}${LOG_ESC_BOLD} $1 ${LOG_ESC_RESET}"
 }
 
 # Log crítico (saída de erro + encerra script)
 log::critical() {
     echo -e "[$(date "+%Y-%m-%d %H:%M:%S")] CRITICAL: $1" >&3
-    echo -e "${LOG_ESC_BOLD_RED}CRITICAL: $1 ${LOG_ESC_RESET}" >&2
+    echo -e "${LOG_ESC_BOLD_RED}CRITICAL: $1 ${LOG_ESC_RESET}"
 }
 
 # Input de usuário. Armazena o valor do input na variável passada como parâmetro.
@@ -75,7 +75,7 @@ log::input() {
     local __varname=$1
     shift
     echo -ne "[$(date "+%Y-%m-%d %H:%M:%S")] >>> $1" >&3
-    echo -ne "${LOG_ESC_BOLD_MAGENTA}>>>${LOG_ESC_RESET}${LOG_ESC_BOLD} $* ${LOG_ESC_RESET}" >&2
+    echo -ne "${LOG_ESC_BOLD_MAGENTA}>>>${LOG_ESC_RESET}${LOG_ESC_BOLD} $* ${LOG_ESC_RESET}"
     read -r user_input
     echo -e "$user_input" >&3
     printf -v "$__varname" '%s' "$user_input"
